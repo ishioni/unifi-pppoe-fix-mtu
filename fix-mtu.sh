633032e3650870d90d287549a2f98b37157fef88
Piotr Maksymiuk <piotr.maksymiuk@movishell.pl>
2026-02-15 00:32:29 +0100

    one more addition

--- fix-mtu.sh
+++ fix-mtu.sh
@@ -19,11 +19,12 @@ INTERFACE_MTU=$(cat "$MTUPATH")
 
 if [ "$INTERFACE_MTU" -ne $MTU ]; then
   echo "MTU for ${PPP_INTERFACE} is $INTERFACE_MTU, changing to $MTU"
-  sed -i "s/ 1492/ ${MTU}/g" "/etc/ppp/peers/${PPP_INTERFACE}"
+  sed -i "s/ ${INTERFACE_MTU}/ ${MTU}/g" "/etc/ppp/peers/${PPP_INTERFACE}"
   ip link set dev ${WAN_INTERFACE} mtu $(( MTU + 8 ))
   ip link set dev ${WAN_INTERFACE}.${VLAN_ID} mtu $(( MTU + 8 ))
-  ifconfig ${WAN_INTERFACE} down
-  ifconfig ${WAN_INTERFACE} up
+  # This might not even be needed?
+  # ifconfig ${WAN_INTERFACE} down
+  # ifconfig ${WAN_INTERFACE} up
   killall pppd
   sleep 1
   killall -HUP dnscrypt-proxy dnsmasq
